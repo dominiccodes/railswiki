08244c8165f04aba0208c4abbedb667c1be8dde5
dominiccodes <email@domcodes.com>
2017-07-26 19:28:09 +0100

    added categories to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,13 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
-  end
+		if params[:category].blank?
+    	@articles = Article.all
+		else
+			@category_id= Category.find_by(name: params[:category]).id
+			@articles = Article.where(category_id: @category_id)			
+  	end
+	end
 
   # GET /articles/1
   # GET /articles/1.json
@@ -70,6 +75,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body, :image_url)
+      params.require(:article).permit(:title, :body, :image_url, :category_id)
     end
 end
